5e704072949625dc03ffedcc68325d29b63c6cb1
pi <pi@raspberrypi>
2023-01-19 15:22:50 +0800

    支持 "内核名称" 支持 "内核版本" 支持 "操作系统" 支持 "系统架构"

--- sysinfo.sh
+++ sysinfo.sh
@@ -2,8 +2,16 @@
 
 print_info(){
     printf "\033[0;30;45m---- 系统信息 (%s) ----\033[0m\n" "$(date +"%Y-%m-%d %H:%M:%S")"
-
+    print_host_info
 }
+
+print_host_info(){
+    p "内核名称" "$(uname -s)"
+    p "内核版本" "$(uname -r)"
+    p "操作系统" "$(uname -o)"
+    p "系统架构" "$(uname -m)"
+}
+
 
 p(){
     # 打印信息
