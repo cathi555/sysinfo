a9de18b9081c3e4d04e80fee18598a73493de6fb
pi <pi@raspberrypi>
2023-01-19 15:37:48 +0800

    支持 "主机型号"

--- sysinfo.sh
+++ sysinfo.sh
@@ -6,12 +6,21 @@ print_info(){
 }
 
 print_host_info(){
+    local model
+    model="$(get_host_model)"
+    if [[ "${model}" ]]
+    then
+        p "主机型号" "${model}"
+    fi
     p "内核名称" "$(uname -s)"
     p "内核版本" "$(uname -r)"
     p "操作系统" "$(uname -o)"
     p "系统架构" "$(uname -m)"
 }
 
+get_host_model(){
+    grep "^Model" /proc/cpuinfo |cut -d: -f2 |sed 's/^ //g'
+}
 
 p(){
     # 打印信息
